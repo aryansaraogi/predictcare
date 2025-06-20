ae75b4f239f4359e5408ff03ed0f90838d49f536
Aryan Saraogi <aryansaraogi02@gmail.com>
2025-06-20 17:34:31 +0530

    source - distination change

--- v0-user-next.config.mjs
+++ v0-user-next.config.mjs
@@ -25,7 +25,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
+        destination: 'https://backend-h19q.onrender.com/:path*', // Proxy to Backend
       },
     ]
   },
